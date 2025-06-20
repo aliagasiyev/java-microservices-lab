0a899d7ea435b49b777041e0e11723c4816aa239
aliagasiyev <agasiyevali2@gmail.com>
2025-06-21 00:10:04 +0400

    Initial project setup: base structure for Java microservices lab

---
+++ src/main/java/az/edu/javamicroserviceslab/JavaMicroservicesLabApplication.java
@@ -0,0 +1,13 @@
+package az.edu.javamicroserviceslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMicroservicesLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMicroservicesLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/edu/javamicroserviceslab/JavaMicroservicesLabApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.javamicroserviceslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMicroservicesLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
